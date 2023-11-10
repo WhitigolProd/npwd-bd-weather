5319e6924d6d336415f24693ed3313510a1a825f
Whitigol <whitetigerwolfthe1st@gmail.com>
2023-11-10 14:46:34 -0500

    🎉 Push version check

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 fx_version "cerulean"
 game "gta5"
-version "1.0"
+version "1.0.0"
 author "Whitigol Web Design"
 
 client_script 'dist/client.js'
--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,32 @@
+import axios from 'axios';
+
 const hotReloadConfig = {
   resourceName: GetCurrentResourceName(),
   files: ['/dist/server.js', '/dist/client.js', '/dist/html/index.js'],
 };
+
+/* Version Check */
+const VERSION_URL: string =
+  'https://raw.githubusercontent.com/WhitigolProd/auto-update-tmp/main/package.json';
+const CURRENT_VERSION: string = GetResourceMetadata(GetCurrentResourceName(), 'version', 0);
+
+axios
+  .get(VERSION_URL)
+  .then((res) => {
+    if (res.data.version > CURRENT_VERSION) {
+      console.log(
+        `^1NPWD Big Daddy Weather is outdated!^7 ^3${CURRENT_VERSION}^7 -> ^2${res.data.version}^7`,
+      );
+      console.log(
+        `Download the latest version at ^5https://github.com/WhitigolProd/npwd-bd-weather/releases/latest/download/npwd_BigDaddy_Weather.zip ^7`,
+      );
+    } else {
+      console.log(`^2NPWD Big Daddy Weather is up to date!^7`);
+    }
+  })
+  .catch((err) => {
+    console.log(`^1Failed to check for updates!^7`);
+  });
 
 console.log('NPWD Weather Forecast for BigDady Weather by Whitigol Web Design');
 
